c375117d2ab37467e42be64cbb7723631f9f1bcf
Mantas <Mantasb77@gmail.com>
2025-10-16 22:38:11 -0600

    Added Coil and Material Icon dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
 
     implementation(libs.dagger.hilt.android)
     ksp(libs.dagger.hilt.android.compiler)
@@ -58,6 +59,8 @@ dependencies {
 
     implementation(libs.bundles.network)
     ksp(libs.moshi.codegen)
+
+    implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
 
