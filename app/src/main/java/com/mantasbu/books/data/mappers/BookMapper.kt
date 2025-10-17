55e73ba0858eae0b2f32da634e9008d85a459e61
Mantas <Mantasb77@gmail.com>
2025-10-17 10:41:46 -0600

    Map DTO to Entity and Entity to Domain

--- app/src/main/java/com/mantasbu/books/data/mappers/BookMapper.kt
+++ app/src/main/java/com/mantasbu/books/data/mappers/BookMapper.kt
@@ -1,5 +1,8 @@
 package com.mantasbu.books.data.mappers
 
+import com.mantasbu.books.data.local.entities.BookDetailsEntity
+import com.mantasbu.books.data.local.entities.BookEntity
+import com.mantasbu.books.data.local.entities.BookListEntity
 import com.mantasbu.books.data.remote.dtos.BookDetailsDto
 import com.mantasbu.books.data.remote.dtos.BookDto
 import com.mantasbu.books.data.remote.dtos.BookListDto
@@ -7,19 +10,47 @@ import com.mantasbu.books.domain.models.Book
 import com.mantasbu.books.domain.models.BookDetails
 import com.mantasbu.books.domain.models.BookList
 
-fun BookDto.toDomain() = Book(
+// DTO -> Entity
+fun BookListDto.toEntity(now: Long) = BookListEntity(
+    id = id,
+    title = title,
+    updatedAt = now,
+)
+
+fun BookDto.toEntity(now: Long) = BookEntity(
+    id = id,
+    listId = listId,
+    title = title,
+    img = img,
+    updatedAt = now,
+)
+
+fun BookDetailsDto.toEntity(now: Long) = BookDetailsEntity(
+    id = id,
+    listId = listId,
+    title = title,
+    img = img,
+    author = author,
+    isbn = isbn,
+    publicationDate = publicationDate,
+    description = description,
+    updatedAt = now,
+)
+
+// Entity -> Domain
+fun BookListEntity.toDomain() = BookList(
+    id = id,
+    title = title,
+)
+
+fun BookEntity.toDomain() = Book(
     id = id,
     listId = listId,
     title = title,
     img = img,
 )
 
-fun BookListDto.toDomain() = BookList(
-    id = id,
-    title = title,
-)
-
-fun BookDetailsDto.toDomain() = BookDetails(
+fun BookDetailsEntity.toDomain() = BookDetails(
     id = id,
     listId = listId,
     title = title,
